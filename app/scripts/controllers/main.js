9bf693ae0c02cbee293c086658d7d15e058c9524
sidona <sidona_g@yahoo.com>
2016-03-02 12:49:32 +0200

    test AngularJs

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,92 +8,105 @@
  * Controller of the testAngularApp
  */
 angular.module('testAngularApp')
-  .controller('MainCtrl', function ($scope,$window) {
+  .controller('MainCtrl', function ($scope) {
 
-    var date=[];
-    $scope.dataTitre1 = {
-      select: 'I',
-      numerotation: [
-        {id: 'I', value: 'I'},
-        {id: '1', value: '1'},
-        {id: 'A', value: 'A'}
-      ],
-     prefixe1:null,
-      suffixe1:null
-    },
-      $scope.dataTitre2 = {
-        select: 'I',
-        numerotation: [
-          {id: 'I', value: 'I'},
-          {id: '1', value: '1'},
-          {id: 'A', value: 'A'}
-        ],
-        prefixe2:null,
-        suffixe2:null
+
+    $scope.data = [
+      {
+        id: 1,
+        name: "Titre 1",
+        numero: {
+          select: 'I',
+          numerotation: [
+            {id: 'I', value: 'I'},
+            {id: '1', value: '1'},
+            {id: 'A', value: 'A'}
+          ],
+          prefixe: '',
+          suffixe: ' '
+        }
       },
-      $scope.dataTitre3 = {
-        select: '1',
-        numerotation: [
-          {id: '1', value: '1'},
-          {id: 'a', value: 'a'},
-          {id: 'i', value: 'i'},
-        ],
-        prefixe3:null,
-        suffixe3:null
+      {
+        id: 2,
+        name: "Titre 2",
+        numero: {
+          select: 'I',
+          numerotation: [
+            {id: 'I', value: 'I'},
+            {id: '1', value: '1'},
+            {id: 'A', value: 'A'}
+          ],
+          prefixe: null,
+          suffixe: null
+        }
       },
-      $scope.dataTitre4 = {
-        select: '1',
-        numerotation: [
-          {id: '1', value: '1'},
-          {id: 'a', value: 'a'},
-          {id: 'i', value: 'i'},
-        ],
-        prefixe4:null,
-        suffixe4:null
+      {
+        id: 3,
+        name: "Titre 3",
+        numero: {
+          select: '1',
+          numerotation: [
+            {id: '1', value: '1'},
+            {id: 'a', value: 'a'},
+            {id: 'i', value: 'i'}
+          ],
+          prefixe: null,
+          suffixe: null
+        }
+      },
+      {
+        id: 4,
+        name: "Titre 4",
+        numero: {
+          select: '1',
+          numerotation: [
+            {id: '1', value: '1'},
+            {id: 'a', value: 'a'},
+            {id: 'i', value: 'i'}
+          ],
+          prefixe: null,
+          suffixe: null
+        }
+      },
+      {
+        id: 5,
+        name: "Titre 5",
+        numero: {
+          select: '1',
+          numerotation: [
+            {id: '1', value: '1'},
+            {id: 'a', value: 'a'},
+            {id: 'i', value: 'i'}
+          ],
+          prefixe: null,
+          suffixe: null
+        }
+      },
+      {
+        id: 6,
+        name: "Titre 6",
+        numero: {
+          select: '1',
+          numerotation: [
+            {id: '1', value: '1'},
+            {id: 'a', value: 'a'},
+            {id: 'i', value: 'i'}
+          ],
+          prefixe: null,
+          suffixe: null
+        }
       }
+    ];
 
-      $scope.dataTitre5 =  {
-        select: '1',
-      numerotation: [
-      {id: '1', value: '1'},
-      {id: 'a', value: 'a'},
-      {id: 'i', value: 'i'}
-    ],
-      prefixe5:null,
-      suffixe5:null
-  }
-      $scope.dataTitre6 =  {
-        select: '1',
-      numerotation: [
-      {id: '1', value: '1'},
-      {id: 'a', value: 'a'},
-      {id: 'i', value: 'i'}
-    ],
-      prefixe6:null,
-      suffixe6:null
-  }
 
-      $scope.saveData = function () {
+    $scope.saveData = function () {
+      console.log(JSON.stringify($scope.data))
+    };
 
-        date=date.concat($scope.dataTitre1,$scope.dataTitre2,$scope.dataTitre3,$scope.dataTitre4,$scope.dataTitre5,$scope.dataTitre6);
-       // $scope.data = JSON.stringify(date);
-        console.log(JSON.stringify(date))
-      }
+    $scope.original = angular.copy($scope.data);
 
-      $scope.cancel=function(){
-
-      $scope.dataTitre1.prefixe1=null;
-      $scope.dataTitre2.prefixe2=null;
-      $scope.dataTitre3.prefixe3 = null;
-      $scope.dataTitre4.prefixe4 = null;
-      $scope.dataTitre5.prefixe5 = null;
-      $scope.dataTitre6.prefixe6 = null;
-
-      $scope.dataTitre1.suffixe1 = null;
-      $scope.dataTitre2.suffixe2 = null;
-      $scope.dataTitre3.suffixe3 = null;
-      $scope.dataTitre4.suffixe4 = null;
-      $scope.dataTitre5.suffixe5 = null;
-      $scope.dataTitre6.suffixe6 = null;
+    $scope.cancel = function () {
+      $scope.data = $scope.original;
     }
+    $scope.cancel();
   });
